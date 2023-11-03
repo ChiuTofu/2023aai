841b9bc9966b02288280656b14a28792d8060876
ChiuTofu <kevin.chiu0723@gmail.com>
2023-11-03 10:02:38 +0800

    week08

---
+++ week08/week08_1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+
+            if n<=0:
+                 return False
+            while n>1: #到一為止
+                if n%3 !=0: #沒辦法被3整除
+                    return False #失敗
+                else:
+                    n = n // 3
+
+        #經過上面的 while迴圈層層檢查，都沒失敗
+            return True    #那就是成功
